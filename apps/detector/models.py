5958d364179a2967838de08fc20058900df6551a
Takae Kido <technopop0614@gmail.com>
2025-08-22 11:04:23 +0900

    Fix models.py

--- apps/detector/models.py
+++ apps/detector/models.py
@@ -6,7 +6,7 @@ class UserImage(db.Model):
     __tablename__ = "user_images"
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(
-        db.Integer, db.ForeignKey("users.id"), nullable=False, index=True
+        db.String, db.ForeignKey("users.id"), nullable=False, index=True
     )
     image_path = db.Column("image_pah", db.String)
     is_detected = db.Column(db.Boolean, default=False)
